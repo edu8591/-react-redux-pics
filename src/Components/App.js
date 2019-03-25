3195d93784719272707fdfeb689e69567a980b26
Eduardo Serrano <eduardoserrano8591@gmail.com>
2019-03-24 20:34:27 -0500

    components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
@@ -12,8 +13,16 @@ import SearchBar from './SearchBar';
 // };
 
 class App extends React.Component {
-  onSearchSubmit= (term) => { 
-    console.log(term)
+  state = { images: [] };
+
+  async onSearchSubmit (term)  { 
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 2a2240d3f10ad3b951ba710dd7e3a108549b97688b24de511408ff188bc9375c'
+      }
+    });
+    this.setState({ images:response.data.results });
   }
    /*term refers to the search term, we should pass on this method to the searchbar as a prop */
 
@@ -21,6 +30,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
